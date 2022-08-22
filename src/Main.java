bfe298998032227da49dd0ca13cae53971aa7b60
Jul1nh0 <108277816+Jul1nh0@users.noreply.github.com>
2022-08-22 14:30:34 +0300

    javal2

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 85423;
+        int miles = ticket / 20;
+        System.out.println("Получено миль: " + miles);
+    }
+}
